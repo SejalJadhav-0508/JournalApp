5d17a663e58df95c992974989c072b97a795ab2b
SejalJadhav-0508 <sejaljadhav0508@gmail.com>
2025-01-15 21:37:13 +0530

    created cron jobs

---
+++ src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
@@ -0,0 +1,48 @@
+package net.engineeringdigest.journalApp.scheduler;
+
+import net.engineeringdigest.journalApp.cache.AppCache;
+import net.engineeringdigest.journalApp.entity.JournalEntity;
+import net.engineeringdigest.journalApp.entity.UserEntity;
+import net.engineeringdigest.journalApp.repository.UserRepositoryImpl;
+import net.engineeringdigest.journalApp.service.EmailService;
+import net.engineeringdigest.journalApp.service.SentimentAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserScheduler {
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private AppCache appCache;
+
+    @Scheduled(cron = "0 0 9 * * SUN")
+    public void fetchUsersAndSendMail(){
+        List<UserEntity> users = userRepository.getUsersForSA();
+        for (UserEntity user : users){
+            List<JournalEntity> journalEntries = user.getJournalEntities();
+            List<String> filteredJournalEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList());
+            String entry = String.join(" ", filteredJournalEntries);
+            String sentiment = sentimentAnalysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", sentiment);
+        }
+    }
+
+    @Scheduled(cron = "0 0/10 * ? * *")
+    public void clearAppCache(){
+        appCache.init();
+    }
+}
